fa9e398d4520d488556a86c0c338c8b72813530b
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-09-21 16:04:16 -0500

    Create main.js content for number branch

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 var num = 9986000;
+var numAddition = 100+100;
+var numConcatOne = 20;
+var numConcatTwo = "80000"
 
 console.log(num);
+console.log(numAddition);
+console.log(numConcatOne + numConcatTwo);
+
+console.log(num + " minutes. That's like watching Die Hard" + " " +numConcatTwo + " times");
